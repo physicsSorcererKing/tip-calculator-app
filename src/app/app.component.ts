e745cbde1c7493fb790adefa140cde27d263b221
physicsSorcererKing <psk@psk.rocks>
2025-02-08 17:11:43 +0800

    chore: use tailwind css and division the webpage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '@/app/header/header.component';
+import { CalculatorComponent } from '@/app/calculator/calculator.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, CalculatorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
